5e4c4ade09ed71b0bb6ec28d543e455aca5866cb
Isa <Isa@MacBook-Pro-de-Isabel.local>
2017-05-08 23:41:09 +0200

    Despliegue en Heroku

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 
+
 var index = require('./routes/index');
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(partials());
 
 app.use('/', index);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
